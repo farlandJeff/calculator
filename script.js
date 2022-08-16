7750f9ea2104e6fabb5b253957e42514ff62196c
Jeff <farlandjeff@gmail.com>
2022-08-16 18:28:36 -0400

    Create operate function

--- script.js
+++ script.js
@@ -13,3 +13,10 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(a, b, op) {
+    if (op == '+') return add(a, b);
+    if (op == '-') return subtract(a, b);
+    if (op == '*') return multiply(a, b);
+    if (op == '/') return divide(a, b);
+}
