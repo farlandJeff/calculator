e95861840fdd381e79cfd2bf4dac3c4bda99461d
Jeff <farlandjeff@gmail.com>
2022-08-18 22:03:20 -0400

    Add AC functionality

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ display.textContent = dNum;
 let num1 = 0;
 let num2 = 0;
 let operator = '';
-let switchNum = false;
 
 const numBtns = document.querySelectorAll('button');
 numBtns.forEach(btn => {
@@ -41,7 +40,12 @@ numBtns.forEach(btn => {
                 display.textContent = num2;
             }
         }
-
+        if (e.target.textContent == 'AC') {
+            num1 = 0;
+            num2 = 0;
+            operator = '';
+            display.textContent = 0;
+        }
     });
 });
 
